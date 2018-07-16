8e3f957596a41edd1b8dd577ac113b25c82511f7
TheGuyLoveNY <kalania@uwindsor.ca>
2018-07-16 07:11:14 +0530

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace WeatherApplication
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(346, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Weather";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "City";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(59, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(281, 24);
+            this.textBox1.TabIndex = 2;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Cloudy.png");
+            this.imageList1.Images.SetKeyName(1, "Foggy.png");
+            this.imageList1.Images.SetKeyName(2, "Mostly Sunny.png");
+            this.imageList1.Images.SetKeyName(3, "Partly Sunny.png");
+            this.imageList1.Images.SetKeyName(4, "Rain.png");
+            this.imageList1.Images.SetKeyName(5, "Snow.png");
+            this.imageList1.Images.SetKeyName(6, "Storms.png");
+            this.imageList1.Images.SetKeyName(7, "Sunny.png");
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(153, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 65);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "20";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(160, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 23);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "WindLabel";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(160, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 23);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "HumidtyLabel";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WeatherApplication.Properties.Resources.Cloudy;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 52);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(131, 128);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(376, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 65);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "F";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(465, 192);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Weather";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace WeatherApplication
+{
+    public partial class mainForm : Form
+    {
+        public TextBox cityBox;
+        public RichTextBox fetchedBox;
+        public PictureBox weatherIcon;
+
+        private ImageList weatherIcons;
+        private Label temperature;
+        private Label wind;
+        private Label humidity;
+
+        private Button unitsButton;
+
+        private string city = "London";
+        private string api = "http://api.openweathermap.org/data/2.5/weather?q=";
+        private string apiKey = "&APPID=742535328578319c60a7f3a5245f164c";
+        private string unit = "&units=metric";
+        private string url = "";
+
+        private string jsonData = "";
+        private string weatherTitle = "";
+        private string windSpeed = "";
+        private string humidtyInfo = "";
+        private string temperatureInfo = "";
+
+        private readonly string celius = "°C";
+        private readonly string fahrenhiet = "°F";
+        private readonly string metre = " km/h";
+        private readonly string miles = " mph";
+
+        private dynamic dataObject;
+        private bool isMetric = true;
+
+        public mainForm()
+        {
+            InitializeComponent();
+            if (CheckInternetConnection())
+            {
+                Setter();
+                GetWeatherInfo();
+            }
+            else
+            {
+                MessageBox.Show("You are offline.");
+                return;
+            }
+
+        }
+
+        private void Setter()
+        {
+            cityBox = textBox1;
+            cityBox.Text = "Windsor";
+
+            weatherIcons = imageList1;
+            weatherIcon = pictureBox1;
+
+            temperature = label3;
+            wind = label4;
+            humidity = label5;
+            unitsButton = button2;
+
+            unitsButton.Text = isMetric ? fahrenhiet : celius;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetWeatherInfo();
+        }
+
+        private void GetWeatherInfo()
+        {
+            if (CheckInternetConnection())
+            {
+                GetData();                              //Fetches the Json Data
+                SetData();                              //Sets the Data
+                if (isMetric)
+                    DisplayDataInMetric();
+                else
+                    DisplayDataInImperial();
+                choosePicture(weatherTitle);
+            }
+            else
+                MessageBox.Show("You are offline.");
+        }
+
+        private void GetData()
+        {
+            city = cityBox.Text;
+            url = api + city + apiKey + unit;
+
+            //Download the string from the given url.
+            using (WebClient wc = new WebClient())
+            {
+                try
+                {
+                    var json = wc.DownloadString(url);
+                    jsonData = json;
+                }
+                catch (Exception Ex)
+                {
+                    MessageBox.Show(Ex.Message);
+                    return;
+                }
+
+            }
+            dataObject = JsonConvert.DeserializeObject(jsonData);
+        }
+
+        //Extract those data and set them to appropriate Texts.
+        private void SetData()
+        {
+            weatherTitle = dataObject.weather[0].description;
+            windSpeed = dataObject.wind.speed;
+            humidtyInfo = dataObject.main.humidity;
+            temperatureInfo = dataObject.main.temp;
+        }
+
+
+        private void DisplayDataInMetric()
+        {
+            wind.Text = "Wind: " + windSpeed + metre;
+            temperature.Text =  temperatureInfo + celius;
+            humidity.Text = "Humidity: " + humidtyInfo + "%";
+
+        }
+
+        private void DisplayDataInImperial()
+        {
+            wind.Text = "Wind: " + windSpeed + miles;
+            temperature.Text = temperatureInfo + fahrenhiet;
+            humidity.Text = "Humidity: " + humidtyInfo + "%";
+
+        }
+
+        private void choosePicture(String weatherTitle)
+        {
+            switch (weatherTitle)
+            {
+                case "clear sky":
+                    weatherIcon.Image = weatherIcons.Images[7];
+                    break;
+
+                case "few clouds":
+                    weatherIcon.Image = weatherIcons.Images[0];
+                    break;
+
+                case "scattered clouds":
+                    weatherIcon.Image = weatherIcons.Images[0];
+                    break;
+
+                case "broken clouds":
+                    weatherIcon.Image = weatherIcons.Images[0];
+                    break;
+
+                case "shower rain":
+                    weatherIcon.Image = weatherIcons.Images[4];
+                    break;
+
+                case "rain" :
+                    weatherIcon.Image = weatherIcons.Images[4];
+                    break;
+
+                case "drizzle":
+                    weatherIcon.Image = weatherIcons.Images[4];
+                    break;
+
+                case "thunderstorm":
+                    weatherIcon.Image = weatherIcons.Images[6];
+                    break;
+
+                case "snow":
+                    weatherIcon.Image = weatherIcons.Images[5];
+                    break;
+
+                case "mist":
+                    weatherIcon.Image = weatherIcons.Images[1];
+                    break;
+
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (isMetric)
+            {
+                isMetric = false;
+                unitsButton.Text = celius;
+                unit = "&units=imperial";
+                GetWeatherInfo();
+            }
+            else
+            {
+                isMetric = true;
+                unitsButton.Text = fahrenhiet;
+                unit = "&units=metric";
+                GetWeatherInfo();
+            }
+        }
+
+        private bool CheckInternetConnection()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (client.OpenRead("http://clients3.google.com/generate_204"))
+                {
+                    return true;
+                }
+
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
